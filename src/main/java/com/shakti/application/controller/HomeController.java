2bd1e7d0d0df80ea13150f00a51a7e36d04cc0fe
User <shaktibhujel06@gmail.com>
2018-11-14 18:09:31 +0545

    controller updated

--- src/main/java/com/shakti/application/controller/HomeController.java
+++ src/main/java/com/shakti/application/controller/HomeController.java
@@ -2,25 +2,31 @@ package com.shakti.application.controller;
 
 import com.shakti.application.controller.dao.ProductDao;
 import com.shakti.application.controller.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class HomeController {
-    private ProductDao productDao = new ProductDao();
+   @Autowired
+    private ProductDao productDao;
 
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model){
+        List<Product> productList = productDao.getAllProducts();
+        model.addAttribute("products", productList);
         return "home";
     }
 
-    @RequestMapping("/productlist")
-    public String getProduct(Model model){
-        List<Product> productList = productDao.getProductList();
-        model.addAttribute("products", productList);
-        return "productList";
+    @RequestMapping("/viewProduct/{productId}")
+    public String viewProducts(@PathVariable String productId, Model model) throws IOException {
+        Product product = productDao.getProductById(productId);
+        model.addAttribute(product);
+        return "viewProduct";
     }
 }
